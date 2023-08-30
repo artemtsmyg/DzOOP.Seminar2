4e8bcb985edaa021971d0cc4297f85de694edd22
Artem Tsmyg <Artemtsmyg@yandex.ru>
2023-08-30 23:06:59 +0300

    First commit

---
+++ src/Classes/PromoClient.java
@@ -0,0 +1,49 @@
+package Classes;
+
+/**
+ * Класс описывающий акционного клиента.
+ */
+public class PromoClient {
+    private String promoName;
+    private int clientId;
+    private static  int participantsCount;
+
+    /**
+     * Конструктор класса PromoClient.
+     *
+     * @param promoName название акции
+     * @param clientId id клиента
+     */
+    public PromoClient(String promoName, int clientId){
+        this.promoName = promoName;
+        this.clientId = clientId;
+        participantsCount++;
+    }
+
+    /**
+     * МЕТОД для получения названия акции.
+     *
+     * @return название акции
+     */
+    public String getPromoName() {
+        return promoName;
+    }
+
+    /**
+     * Метод для получения id клиента.
+     *
+     * @return id клиента
+     */
+    public int getClientId() {
+        return clientId;
+    }
+    /**
+     * Метод для получения количества участников акции.
+     *
+     * @return количество участников акции
+     */
+    public static int getParticipantsCount() {
+        return participantsCount;
+    }
+}
+
---
+++ src/Interfaces/iReturnOrder.java
@@ -0,0 +1,19 @@
+package Interfaces;
+
+/**
+ * Интерфейс описывающий возможность возврата заказа.
+ */
+    public interface iReturnOrder {
+    /**
+     * Метод для возврата заказа.
+     */
+    void returnOrder();
+
+    /**
+     * Метод для проверки возможности возврата заказа.
+     *
+     * @return true, если заказ возможно вернуть, иначе false.
+     */
+        boolean isReturnable();
+    }
+
